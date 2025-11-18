960a1ef3ca5c139037d3be6283dd50dec55e7300
Dev Goswami <goswami.dev2016@gmail.com>
2025-11-18 06:12:49 +0000

    it has to be stick to the bottom of the sceen

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,9 +6,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen w-full flex-col bg-muted/40 overflow-hidden">
+    <div className="flex h-screen w-full flex-col bg-muted/40">
       <DashboardHeader />
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 overflow-y-auto">
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 overflow-hidden">
         {children}
       </main>
     </div>
